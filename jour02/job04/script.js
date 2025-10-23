f81d2bc93f574801ff6712b59ed42971fdaabc1d
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-10-23 14:58:42 +0200

    fix job04 : cleanage du code

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -1,20 +1,13 @@
 const textarea = document.getElementById("keylogger");
-
+console.log(textarea);
 document.addEventListener("keydown", function(event) {
     const key = event.key;
 
     // On vérifie si la touche est une lettre (a-z)
     if (key.length === 1 && key.match(/[a-z]/i)) {
 
-        // Si le focus est dans le textarea
-        if (document.activeElement === textarea) {
-            // Empêche la lettre de s’écrire automatiquement
-            event.preventDefault();
-            // Et on l’ajoute nous-mêmes deux fois
-            textarea.value += key + key;
-        } else {
-            // Si focus ailleurs, ajoute une fois
+
             textarea.value += key;
-        }
+        
     }
 });
