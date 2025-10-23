a54fc4952836906928a4c38a7d1aab8ab9994111
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-10-23 12:03:32 +0200

    fix : style

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jour 02 - job01</title>
+
+ <style>
+        /* cacher l’article */
+        #citation {
+            display: none;
+        }
+    </style>
+
+
 </head>
 <body>
 
     <article id="citation">
-        La vie a beaucoup plus d'imagination que nous
+        La vie a beaucoup plus d'imagination que nous !
     </article>
 
-    <button id="button">Citation du jour</button>
+    <button id="button">Citation du jour dans Console Log</button>
 
     <script src="script.js"></script>
 </body>
